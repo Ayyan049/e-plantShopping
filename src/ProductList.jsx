0fe9374a89710fb8eb4aa7c43141a59adf858750
Ayyan049 <ayyan137a@gmail.com>
2025-01-25 14:13:41 -0500

    commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,8 +274,8 @@ const handlePlantsClick = (e) => {
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                <a   style={{textDecoration:'none'}}>
                         <div>
-                    <h3 style={{color:'white'}}>Ziad's Plants</h3>
-                    <i style={{color:'white'}}>Where Green Meets Serenity</i>
+                    <h3 style={{color:'white'}}>Plants World</h3>
+                    <i style={{color:'white'}}>Where Life begins</i>
                     </div>
                     </a>
                 </div>
